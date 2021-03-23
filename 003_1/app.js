0ae89d4f6d9dd08c8f997565c9996e01fae118c1
Andrew Koshelev <ndrei61999@gmail.com>
2021-03-23 08:46:56 -1000

    Finish this part

--- 003_1/app.js
+++ 003_1/app.js
@@ -1,11 +1,17 @@
 Vue.createApp({
-    data: () => ({
-        title: 'I am Groot',
-        person: {
-            name: 'Meg',
-            last: 'Rew',
-            age: 24
-        },
-        items: [1, 2, 3, 4, 5, 6]
-    })
+    data() {
+        return {
+            title: 'I am Groot',
+            person: {
+                name: 'Meg',
+                last: 'Rew',
+                age: 24
+            },
+            items: [1, 2, 3, 4, 5, 6]
+        }},
+    methods: {
+        // addItem() {
+        //     this.$refs.myInput.value
+        // }
+    }
 }).mount('#app')
